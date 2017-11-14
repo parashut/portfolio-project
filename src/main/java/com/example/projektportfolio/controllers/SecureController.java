2cf242c247da41dc7327fab352fc367f78a95c65
parashut <parashut@gmail.com>
2017-11-14 21:17:13 +0100

    +PageInfoRepository

---
+++ src/main/java/com/example/projektportfolio/controllers/SecureController.java
@@ -0,0 +1,19 @@
+package com.example.projektportfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecureController {
+
+    @GetMapping("/")
+    public String mainPage(){
+        return "index";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "adminview";
+    }
+
+}
