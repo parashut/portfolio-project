8f0819dbd9ec2baaeca7968a8a10c21a476dba17
parashut <parashut@gmail.com>
2017-11-14 22:00:10 +0100

    +security

--- src/main/java/com/example/projektportfolio/WebSecurity.java
+++ src/main/java/com/example/projektportfolio/WebSecurity.java
@@ -1,6 +1,9 @@
 package com.example.projektportfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,5 +11,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    LoginHandler loginHandler;
 
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
+//                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .successHandler(loginHandler)
+//                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+//        httpSecurity.exceptionHandling().accessDeniedPage("/403")
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        //zmienić na jdbc authentication
+        auth.inMemoryAuthentication()
+                .withUser("przemo").password("test1234").roles("ADMIN");
+    }
 }
--- src/main/java/com/example/projektportfolio/controllers/SecureController.java
+++ src/main/java/com/example/projektportfolio/controllers/SecureController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SecureController {
 
-    @GetMapping("/")
-    public String mainPage(){
-        return "index";
-    }
+//    @GetMapping("/")
+//    public String mainPage(){
+//        return "index";
+//    }
 
     @GetMapping("/admin")
     public String adminPage() {
