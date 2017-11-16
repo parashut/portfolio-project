86350071c327d2cc60211a85c24aa0ece22f090b
parashut <parashut@gmail.com>
2017-11-16 23:02:26 +0100

    +logout

--- src/main/java/com/example/projektportfolio/controllers/SecureController.java
+++ src/main/java/com/example/projektportfolio/controllers/SecureController.java
@@ -1,7 +1,13 @@
 package com.example.projektportfolio.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class SecureController {
@@ -14,6 +20,15 @@ public class SecureController {
     @GetMapping("/admin")
     public String adminPage() {
         return "adminview";
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
     }
 
 }
