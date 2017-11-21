2efa654277f6ac10299a80fa721b58535baf70db
parashut <parashut@gmail.com>
2017-11-22 00:28:57 +0100

    +adminview under construction

--- src/main/java/com/example/projektportfolio/controllers/SecureController.java
+++ src/main/java/com/example/projektportfolio/controllers/SecureController.java
@@ -1,10 +1,15 @@
 package com.example.projektportfolio.controllers;
 
+import com.example.projektportfolio.PageInfoRepository;
+import com.example.projektportfolio.ProjektportfolioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,8 +22,16 @@ public class SecureController {
 //        return "index";
 //    }
 
-    @GetMapping("/admin")
-    public String adminPage() {
+    @Autowired
+    ProjektportfolioRepository projektportfolioRepository;
+    @Autowired
+    PageInfoRepository pageInfoRepository;
+
+    @RequestMapping("/admin")
+    public String adminPage(Model model) {
+        model.addAttribute("projects", projektportfolioRepository.findAll());
+        model.addAttribute("informations", pageInfoRepository.findAll());
+
         return "adminview";
     }
 
