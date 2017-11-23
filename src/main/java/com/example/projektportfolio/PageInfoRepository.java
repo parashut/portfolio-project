b961723cefedd9527a091fed750287e5786446dd
draminskip <przemyslaw.draminski@kantar.com>
2017-11-23 14:54:33 +0100

    +progress on admin page

--- src/main/java/com/example/projektportfolio/PageInfoRepository.java
+++ src/main/java/com/example/projektportfolio/PageInfoRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface PageInfoRepository extends CrudRepository<PageInfo, Integer> {
     List<PageInfo> findAll();
+    PageInfo findById(int id);
 }
--- src/main/java/com/example/projektportfolio/controllers/SecureController.java
+++ src/main/java/com/example/projektportfolio/controllers/SecureController.java
@@ -2,6 +2,7 @@ package com.example.projektportfolio.controllers;
 
 import com.example.projektportfolio.PageInfoRepository;
 import com.example.projektportfolio.ProjektportfolioRepository;
+import com.example.projektportfolio.models.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,6 +10,8 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,11 +19,6 @@ import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class SecureController {
-
-//    @GetMapping("/")
-//    public String mainPage(){
-//        return "index";
-//    }
 
     @Autowired
     ProjektportfolioRepository projektportfolioRepository;
@@ -31,8 +29,16 @@ public class SecureController {
     public String adminPage(Model model) {
         model.addAttribute("projects", projektportfolioRepository.findAll());
         model.addAttribute("informations", pageInfoRepository.findAll());
+        model.addAttribute("pageInfo", new PageInfo());
 
         return "adminview";
+    }
+
+    @PostMapping("/admin")
+    public String editInfo(@ModelAttribute PageInfo pageInfo){
+        pageInfoRepository.save(pageInfoRepository.findById(pageInfo.getId()));
+
+        return "redirect:/admin";
     }
 
     @GetMapping("/logout")
